8b6998a7ea67d1fd128c523ae445044b00c7364d
Angela Anjorin <angela.anjorin@gmail.com>
2024-07-17 10:16:37 +0000

    update the templated for the signup, login and base and post_detail htmls
    Update the about app

--- about/admin.py
+++ about/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import About
 
 
-admin.site.register(About)
+@admin.register(About)
+class AboutAdmin(SummernoteModelAdmin):
+    """
+    Adds rich-text editing of content in admin
+    """
+    summernote_fields = ('content',)
+
+    """
+    Lists message and read fields for display in admin
+    """
+    #list_display = ('message', 'read',)
--- about/models.py
+++ about/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+
 
 # Create your models here.
 
@@ -8,8 +10,9 @@ class About(models.Model):
     Stores a single about me text
     """
     title = models.CharField(max_length=200)
-    updated_on = models.DateTimeField(auto_now=True)
+    featured_image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
